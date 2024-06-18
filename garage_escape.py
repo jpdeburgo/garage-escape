80d69b4acb0b4cb2ee83aa267cbff76706d2f7b7
Justin De Burgo <justin.deburgo@aa.com>
2024-06-18 14:55:43 -0500

    submitted score: 2268

--- garage_escape.py
+++ garage_escape.py
@@ -1,8 +1,6 @@
-from print_path import print_path
 def bfs_find_one(array_2d, start_col, start_row, find_exit=False):
     rows, cols = len(array_2d), len(array_2d[0]) 
     visited = [[False for _ in range(cols)] for _ in range(rows)]
-    previous = [[None for _ in range(cols)] for _ in range(rows)]  # To store the path
     moves = 0
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
     queue = [((start_row, start_col), moves)]
@@ -10,17 +8,14 @@ def bfs_find_one(array_2d, start_col, start_row, find_exit=False):
     while queue:
         (row, col), moves = queue.pop(0)
         if not find_exit and array_2d[row][col] == 1: 
-            print_path(previous, (start_row, start_col), (row, col))
             return [[col, row], moves]
         elif find_exit and col == len(array_2d[0]) - 1 and row == 0:
-            print_path(previous, (start_row, start_col), (row, col))
             return [[col, row], moves]
         for dr, dc in directions:
             new_row, new_col = row + dr, col + dc
             if 0 <= new_row < rows and 0 <= new_col < cols and not visited[new_row][new_col] and array_2d[new_row][new_col] != 4:
                 queue.append(((new_row, new_col), moves + 1))
                 visited[new_row][new_col] = True
-                previous[new_row][new_col] = (row, col)
     return None
 garage_escape_file = open("randall-tramia-cases.txt")
 curr_garage = []
