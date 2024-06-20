790fa6a5ed378276df506f8939687b989f6a21c5
Justin De Burgo <justin.deburgo@aa.com>
2024-06-20 10:54:07 -0500

    score: 1048

--- garage_escape.py
+++ garage_escape.py
@@ -1,39 +1,19 @@
-def bfs(array_2d,start_col,start_row,find_exit=False):
- rows,cols=len(array_2d),len(array_2d[0]) 
- v=[[False for _ in range(cols)] for _ in range(rows)]
- m=0
- d=[(-1,0),(1,0),(0,-1),(0,1)]
- q=[(start_row,start_col,m)]
- v[start_row][start_col]=True
+l,lines,c_g,c_f,s_i,g_m=len,open("example.txt").readlines(),[],[],[-1,-1,-1],0
+def bfs(arr,s_c,s_r,f_e='F'):
+ rows,cols=l(arr),l(arr[0]);v,m,d=[['F' for _ in range(cols)] for _ in range(rows)],0,[(-1,0),(1,0),(0,-1),(0,1)];q=[(s_r,s_c,m)];v[s_r][s_c]='T'
  while q:
   row,col,m=q.pop(0)
-  if not find_exit and array_2d[row][col]==1:return[[col,row],m]
-  elif find_exit and col==len(array_2d[0])-1 and row==0:return[[col,row],m]
+  if f_e=='F' and arr[row][col]==1:return[[col,row],m]
+  elif f_e and col==l(arr[0])-1 and row==0:return[[col,row],m]
   for dr,dc in d:
-   nr,nc=row + dr,col + dc
-   if 0<=nr<rows and 0<=nc<cols and not v[nr][nc] and array_2d[nr][nc]!=4:
-    q.append((nr,nc,m+1))
-    v[nr][nc]=True
-file,c_g,c_f,s_i,g_m=open("example.txt"),[],[],[-1,-1,-1],0
-lines=file.readlines()
+   nr,nc=row+dr,col+dc
+   if 0<=nr<rows and 0<=nc<cols and v[nr][nc]=='F'and arr[nr][nc]!=4:q.append((nr,nc,m+1));v[nr][nc]='T'
 for i,line in enumerate(lines):
- if "G" in line or i==len(lines)-1:
-    if c_f:
-     c_g.append(c_f)
-     _,f_m=bfs(c_f,s_i[0],s_i[1],True)
-     g_m+=f_m+1
-     print(f"Escaped in {g_m} moves")
-    s_i,c_g,g_m,c_f=[-1,-1,-1],[],0,[]
- if "F" in line:
-  if c_f:
-   c_g.append(c_f)
-  if s_i[0] != -1: 
-   one_index,f_m=bfs(c_f,s_i[0],s_i[1])
-   s_i,c_f=[one_index[0],one_index[1],s_i[2] + 1],[]
-   g_m+=f_m+1
- if "R" in line:
-  nums=line.split(':')[1].strip()
-  row=[int(x) for x in nums.split(' ')]
-  c_f.append(row)
-  if 3 in row:
-   s_i[0],s_i[1],s_i[2]=row.index(3),len(c_f)-1,len(c_g)
+ if "G"in line or i==l(lines)-1:
+    if c_f:c_g.append(c_f);_,f_m=bfs(c_f,s_i[0],s_i[1],'T');g_m+=f_m+1;print(f"Escaped in {g_m} moves");s_i,c_g,g_m,c_f=[-1,-1,-1],[],0,[]
+ if "F"in line:
+  if c_f:c_g.append(c_f)
+  if s_i[0]!=-1:idx,f_m=bfs(c_f,s_i[0],s_i[1]);s_i,c_f=[idx[0],idx[1],s_i[2] + 1],[];g_m+=f_m+1
+ if "R"in line:
+  c_f.append([int(x) for x in line.split(':')[1].strip().split(' ')])
+  if 3 in c_f[-1]:s_i[0],s_i[1],s_i[2]=c_f[-1].index(3),l(c_f)-1,l(c_g)
